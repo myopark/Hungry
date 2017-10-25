4d5ba8e1475bb765293f20b904a874899f47bf3f
melody park <melodypark@utexas.edu>
2017-10-24 19:30:52 -0500

    init

---
+++ Hungry/Hungry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hungry
+//
+//  Created by Melody park on 10/24/17.
+//  Copyright © 2017 Melody park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
