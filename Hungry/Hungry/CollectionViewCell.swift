34d9fee1515d3f58ee9476e8d1183fd3c4cebcd8
melody park <melodypark@utexas.edu>
2017-10-29 17:31:38 -0500

    skeleton storyboard, BUG: xcode will crash

---
+++ Hungry/Hungry/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Hungry
+//
+//  Created by Melody park on 10/29/17.
+//  Copyright © 2017 Melody park. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
